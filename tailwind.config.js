73e7d2b53bc857b9a597cddb54d6cee4984d9934
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-07-03 11:18:24 +0100

    configured other dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    darkMode: 'class',
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                zinc: {
+                    25: '#858585',
+                },
 
+                blue: {
+                    1000: '#2b3945',
+                    1100: '#202c37',
+                    1200: '#111517',
+                },
+            },
+            fontFamily: {
+                nunito: ['"Nunito Sans"', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
