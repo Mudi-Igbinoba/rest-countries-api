a4215df140b70f5080e93463127ff0dfcc645b6d
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-07-04 20:35:30 +0100

    set up main background and padding

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,22 @@ function App() {
 
     return (
         <div className={styles}>
-            {/* Header */}
-            <Header isDark={isDark} setDark={setIsDark} />
-            {/* Main */}
+            <div className=' text-blue-1200 dark:text-white'>
+                {/* Header */}
+                <Header isDark={isDark} setDark={setIsDark} />
+                {/* Main */}
+                <main className='bg-zinc-50 dark:bg-blue-1100 min-h-screen'>
+                    <div className='container px-4 py-9 md:px-9 min-[1440px]:px-0 min-[1440px]:py-7'>
+                        <div className='flex flex-wrap justify-between items-center'></div>
+                    </div>
+                </main>
 
-            {/* Search Bar */}
+                {/* Search Bar */}
 
-            {/* Filter */}
+                {/* Filter */}
 
-            {/* Cards */}
+                {/* Cards */}
+            </div>
         </div>
     );
 }
