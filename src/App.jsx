82405e3348e5d392d864347ef8378e9d301c01ab
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2023-07-08 19:16:10 +0100

    finished styling the country cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import Dropdown from './components/Dropdown';
+import Cards from './components/Cards';
+import data from './data.json';
 
 function App() {
     const [isDark, setIsDark] = useState(false);
@@ -13,14 +15,26 @@ function App() {
                 {/* Header */}
                 <Header isDark={isDark} setDark={setIsDark} />
                 {/* Main */}
-                <main className='bg-zinc-50 dark:bg-blue-1100 min-h-screen transition-all duration-100'>
-                    <div className='container mx-auto px-4 py-8 md:px-9 min-[1440px]:px-0 lg:py-12'>
-                        <div className='flex flex-wrap justify-between items-center'>
+                <main className='bg-zinc-50 dark:bg-blue-1100 min-h-screen transition-all duration-100 text-sm'>
+                    <section className='container mx-auto px-4 py-6 md:px-9 min-[1440px]:px-0 lg:py-12'>
+                        <section className='flex flex-wrap justify-between items-center'>
                             <SearchBar />
-
                             <Dropdown />
-                        </div>
-                    </div>
+                        </section>
+
+                        <section className='mt-9 lg:mt-14 grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:gap-x-10 gap-y-12 md:gap-y-20'>
+                            {data.map((e) => (
+                                <Cards
+                                    key={e.alpha2Code}
+                                    name={e.name}
+                                    region={e.region}
+                                    flag={e.flag}
+                                    population={e.population}
+                                    capital={e.capital}
+                                />
+                            ))}
+                        </section>
+                    </section>
                 </main>
 
                 {/* Search Bar */}
---
+++ src/components/Cards.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+
+const Cards = ({ name, flag, region, population, capital }) => {
+    return (
+        <div className='w-4/5 sm:w-auto mx-auto sm:mx-0 lg:w-auto rounded-lg shadow-lg bg-white dark:bg-blue-1000 transition-all duration-100'>
+            <div className='lg:h-[250px]'>
+                <img
+                    className='rounded-t-lg w-full h-full'
+                    src={flag}
+                    alt={name}
+                />
+            </div>
+
+            <div className='p-6'>
+                <h2 className='font-extrabold text-base lg:text-lg mb-2.5'>
+                    {name}
+                </h2>
+                <p className='mb-1'>
+                    <strong className='font-semibold me-1'>Population:</strong>
+                    {new Intl.NumberFormat().format(population)}
+                </p>
+                <p className='mb-1'>
+                    <strong className='font-semibold me-1'>Region:</strong>
+                    {region}
+                </p>
+                <p className='mb-1'>
+                    <strong className='font-semibold me-1'>Capital:</strong>
+                    {capital}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+Cards.propTypes = {
+    name: PropTypes.string,
+    flag: PropTypes.string,
+    population: PropTypes.number,
+    region: PropTypes.string,
+    capital: PropTypes.string,
+};
+
+export default Cards;
